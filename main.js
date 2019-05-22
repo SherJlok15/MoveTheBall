19abe91151749337c758fa350c9f6d9dd55f1f35
Fandorin <SherJlok15@gmail.com>
2019-05-22 14:16:55 +0300

    Solution

---
+++ main.js
@@ -0,0 +1,23 @@
+const field = document.querySelector('#field');
+const ball = document.querySelector('#ball');
+
+function move(e) {
+  let fieldPosition = field.getBoundingClientRect();
+
+  let fieldTop = fieldPosition.top + field.clientTop;
+  let fieldLeft = fieldPosition.left + field.clientLeft;
+
+  let ballTop = e.clientY - fieldTop;
+  let ballLeft = e.clientX - fieldLeft;
+
+  if (ballTop < 20) ballTop = 20;
+  if (ballTop > 130) ballTop = 130;
+
+  if (ballLeft < 20) ballLeft = 20;
+  if (ballLeft > 180) ballLeft = 180;
+
+  ball.style.top = ballTop + 'px';
+  ball.style.left = ballLeft + 'px';
+}
+
+field.addEventListener('click', move);
